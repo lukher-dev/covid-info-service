266aef0009a5e8acb45ef4859ed1c591f1b119ac
Luker <luker.plus@gmail.com>
2020-11-04 00:58:30 +0100

    Delete Google Analytics setup :(

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ReactGA from 'react-ga';
 import TopBar from './components/TopBar'
 import Restrictions from './components/restrictions/Restrictions'
 import Statistics from './components/statistics/Statistics'
@@ -10,11 +9,6 @@ import {
 } from "react-router-dom";
 
 function App() {
-  ReactGA.initialize('G-XKCGLLTPXT');
-  history.listen((location) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  })
   return (
     <Router>
       <TopBar />
