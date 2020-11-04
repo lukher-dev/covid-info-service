08c5d0202f1eacc5aa4afa8f742e8aa89a32fc2a
Luker <luker.plus@gmail.com>
2020-11-04 21:44:29 +0100

    Frontend beta

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,11 +4,13 @@ import {
     Link
 } from "react-router-dom";
 import lastUpdateDate from '../data/lastUpdateDate.json'
-import { newOrOld, percentageDifference, updateWarning } from './helpers'
+import { newOrOld, percentageDifference, updateWarning, doomCounterValue } from './helpers'
 import { FaTwitterSquare } from 'react-icons/fa';
 
 function Landing() {
     ReactGa.pageview(window.location.pathname);
+    const [doomCounterValues, doomCounterActualValues, average] = doomCounterValue()
+    console.log(doomCounterValues)
     return (
         <div>
             <Jumbotron className='p-0 m-2' >
@@ -20,39 +22,38 @@ function Landing() {
                         </Col>
                     </Row>
                     <Row>
-                        <Col className='mb-3'>
+                        <Col sm='4' xs={{ span: 6 }} className='mb-3'>
                             <h6>Nowe zakażenia:</h6>
                             <h4 className='m-0'>{newOrOld('new_cases_today')}</h4>
                             {updateWarning('new_cases_today')}
                             {percentageDifference('new_cases_today')}
                         </Col>
-                        <Col className='mb-3'>
-                            <h6>Nowe zgony:</h6>
-                            <h4 className='m-0'>{newOrOld('dead_all_today')}</h4>
-                            {updateWarning('dead_all_today')}
-                            {percentageDifference('dead_all_today')}
-                        </Col>
-                        <Col sm='4' className='mb-3'>
+                        <Col sm='4' xs={{ span: 12, order: 'last' }} className='mb-3'>
                             <h6>Aktywne przypadki:</h6>
                             <h4 className='m-0'>{newOrOld('active_cases')}</h4>
                             {updateWarning('active_cases')}
                             {percentageDifference('active_cases')}
                         </Col>
-                    </Row>
-                    <Row className='mb-3'>
-                        <Col>
-                            <h6>Liczba testów:</h6>
-                            <h4>{newOrOld('tests_done_today')}</h4>
-                            {updateWarning('tests_done_today')}
-                        </Col>
-                        <Col>
-                            <h6>Zgony Łącznie:</h6>
-                            <h4>{newOrOld('dead_global')}</h4>
-                            {updateWarning('dead_global')}
+                        <Col sm={{ span: 4, order: 'last' }} xs={{ span: 6 }} className='mb-3'>
+                            <h6>Nowe zgony:</h6>
+                            <h4 className='m-0'>{newOrOld('dead_all_today')}</h4>
+                            {updateWarning('dead_all_today')}
+                            {percentageDifference('dead_all_today')}
                         </Col>
                     </Row>
                     <Row>
                         <Col>
+                            <hr />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={{ span: 3, order: 1 }} md={{ span: 6, order: 1 }} xs={{ span: 6, order: 1 }}>
+                            <h6>Liczba testów:</h6>
+                            <h4>{newOrOld('tests_done_today')}</h4>
+                            {updateWarning('tests_done_today')}
+                            <br />
+                        </Col>
+                        <Col lg={{ span: 3, order: 2 }} md={{ span: 6, order: 3 }} xs={{ span: 6, order: 3 }}>
                             <h6>Zajęte respiratory:</h6>
                             <h5>{newOrOld('occupied_respirator_count')}/{newOrOld('respirator_count')}</h5>
                             <ProgressBar variant="danger" animated
@@ -60,13 +61,19 @@ function Landing() {
                                 label={String(Math.floor(newOrOld('occupied_respirator_count') / newOrOld('respirator_count') * 100)) + '%'} />
                             {updateWarning('occupied_respirator_count')}
                         </Col>
-                        <Col>
+                        <Col lg={{ span: 3, order: 3 }} md={{ span: 6, order: 4 }} xs={{ span: 6, order: 4 }}>
                             <h6>Zajęte łóżka:</h6>
                             <h5>{newOrOld('occupied_bed_count')}/{newOrOld('bed_count')}</h5>
                             <ProgressBar variant="danger" animated
                                 now={newOrOld('occupied_bed_count') / newOrOld('bed_count') * 100}
                                 label={String(Math.floor(newOrOld('occupied_bed_count') / newOrOld('bed_count') * 100)) + '%'} />
                             {updateWarning('occupied_bed_count')}
+                        </Col>
+                        <Col lg={{ span: 3, order: 4 }} md={{ span: 6, order: 2 }} xs={{ span: 6, order: 2 }}>
+                            <h6>Zgony Łącznie:</h6>
+                            <h4>{newOrOld('dead_global')}</h4>
+                            {updateWarning('dead_global')}
+                            <br />
                         </Col>
                     </Row>
                     <Row>
@@ -77,6 +84,33 @@ function Landing() {
                     <Row>
                         <Col className='mb-2'>
                             <small>Dane pobierane z <a href='https://twitter.com/MZ_GOV_PL'>{<FaTwitterSquare size={20} />}MZ_GOV_PL</a></small>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+            <Jumbotron className='p-0 m-2' >
+                <Container className="text-center" >
+                    <Row>
+                        <Col>
+                            <h6>Etapy zasad bezpieczeństwa</h6>
+                            <p className="font-weight-light">Średnia liczba nowych zakażeń przez ostatnie 7dni na 100 tyś. mieszkańców: <b>{Math.round(average * 1000) / 1000}</b></p>
+                            <ProgressBar className='doom-bar'>
+                                <ProgressBar className="color-green" now={doomCounterValues[0]} label={Math.floor(doomCounterActualValues[0]).toString() + '/10'} />
+                                <ProgressBar className="color-yellow" now={doomCounterValues[1]} label={Math.floor(doomCounterActualValues[1]).toString() + '/25'} />
+                                <ProgressBar className="color-red" now={doomCounterValues[2]} label={Math.floor(doomCounterActualValues[2]).toString() + '/50'} />
+                                <ProgressBar className="color-purple" now={doomCounterValues[3]} label={Math.floor(doomCounterActualValues[3]).toString() + '/70'} />
+                                <ProgressBar className="color-black" now={doomCounterValues[4]} label={Math.floor(doomCounterActualValues[4]).toString()} />
+                            </ProgressBar>
+                            <ProgressBar className='doom-light-bar'>
+                                <ProgressBar className="color-light-green" now={10 / 0.75} />
+                                <ProgressBar className="color-light-yellow" now={15 / 0.75} />
+                                <ProgressBar className="color-light-red" now={25 / 0.75} />
+                                <ProgressBar className="color-light-purple" now={20 / 0.75} />
+                                <ProgressBar className="color-light-black" now={5 / 0.75} />
+                            </ProgressBar>
+                            <hr className='m-2' />
+                            <small>Więcej informacji o etapach zasad bezpieczeństwa:</small>
+                            <p><a href='https://twitter.com/PremierRP/status/1323980694033489923/photo/1'>{<FaTwitterSquare size={20} />}Kancelaria Premiera</a></p>
                         </Col>
                     </Row>
                 </Container>
--- src/components/helpers.js
+++ src/components/helpers.js
@@ -21,3 +21,78 @@ export function percentageDifference(field) {
         return <div className='adnotation text-secondary'>(-%)</div>
     return <div className='adnotation text-success'>({value}%↘)</div>
 }
+
+export function doomCounterValue() {
+    var sum = 0;
+    for (var i = 0; i < statsData.casesHistory.length; i++) {
+        sum += parseInt(statsData.casesHistory[i], 10);
+    }
+    var avg = (sum / statsData.casesHistory.length) / 38383000 * 100000;
+
+    const max = 75 / 100
+    const doomCounterValues = []
+    const doomCounterActualValues = []
+    if (avg < 10) {
+        doomCounterValues.push((avg - 2) / max)
+        doomCounterValues.push(0)
+        doomCounterValues.push(0)
+        doomCounterValues.push(0)
+        doomCounterValues.push(0)
+
+        doomCounterActualValues.push(avg)
+        doomCounterActualValues.push(0)
+        doomCounterActualValues.push(0)
+        doomCounterActualValues.push(0)
+        doomCounterActualValues.push(0)
+    } else if (avg < 25) {
+        doomCounterValues.push(10 / max)
+        doomCounterValues.push((avg - 10) / max)
+        doomCounterValues.push(0)
+        doomCounterValues.push(0)
+        doomCounterValues.push(0)
+
+        doomCounterActualValues.push(10)
+        doomCounterActualValues.push(avg)
+        doomCounterActualValues.push(0)
+        doomCounterActualValues.push(0)
+        doomCounterActualValues.push(0)
+    } else if (avg < 50) {
+        doomCounterValues.push(10 / max)
+        doomCounterValues.push(15 / max)
+        doomCounterValues.push((avg - 25) / max)
+        doomCounterValues.push(0)
+        doomCounterValues.push(0)
+
+        doomCounterActualValues.push(10)
+        doomCounterActualValues.push(25)
+        doomCounterActualValues.push(avg)
+        doomCounterActualValues.push(0)
+        doomCounterActualValues.push(0)
+    } else if (avg < 70) {
+        doomCounterValues.push(10 / max)
+        doomCounterValues.push(15 / max)
+        doomCounterValues.push(25 / max)
+        doomCounterValues.push((avg - 50) / max)
+        doomCounterValues.push(0)
+
+        doomCounterActualValues.push(10)
+        doomCounterActualValues.push(25)
+        doomCounterActualValues.push(50)
+        doomCounterActualValues.push(avg)
+        doomCounterActualValues.push(0)
+    } else {
+        doomCounterValues.push(10 / max)
+        doomCounterValues.push(15 / max)
+        doomCounterValues.push(25 / max)
+        doomCounterValues.push(20 / max)
+        doomCounterValues.push((avg - 70) / max)
+
+        doomCounterActualValues.push(10)
+        doomCounterActualValues.push(25)
+        doomCounterActualValues.push(50)
+        doomCounterActualValues.push(70)
+        doomCounterActualValues.push(avg)
+    }
+
+    return [doomCounterValues, doomCounterActualValues, avg]
+}
