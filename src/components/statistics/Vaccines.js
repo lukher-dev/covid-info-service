b3a74c13c1935b3704b371cb1615fa21865f5974
Łukasz Grygier <luker.plus@gmail.com>
2021-04-14 13:42:35 +0200

    api update

--- src/components/statistics/Vaccines.js
+++ src/components/statistics/Vaccines.js
@@ -9,7 +9,8 @@ function Vaccines() {
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    axios.get('https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_szczepienia_widok2/FeatureServer/0/query?f=json&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2724%27%20HOUR)%20AND%20CURRENT_TIMESTAMP&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard', {
+    console.log('request')
+    axios.get('https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_szczepienia_actual_widok/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true', {
     }).then(response => {
       if(response.data && response.data.features && response.data.features[0].attributes)
         setData(response.data.features[0].attributes)
