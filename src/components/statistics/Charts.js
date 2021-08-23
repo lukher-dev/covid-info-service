634fd0e61beb546f88afad7937d848eb1e7ae8d5
Artur Mostowski <artur.mostowski@currency-one.com>
2021-08-23 15:26:18 +0200

    chart | fix rolling average

--- src/components/statistics/Charts.js
+++ src/components/statistics/Charts.js
@@ -10,10 +10,31 @@ dayjs.extend(customParseFormat)
 
 function Charts() {
   const [shownLimit, setShownLimit] = useState(null)
+  const toDayJs = (x) => dayjs(x.DATA_SHOW, 'DD.MM.YYYY HH:mm')
   const today = dayjs()
-  const shownData = !shownLimit ? historicData : historicData.filter(
-    (day) => today.diff(dayjs(day.DATA_SHOW, 'DD.MM.YYYY HH:mm'), 'day') <= shownLimit
+  const { rollingAverage } = historicData.reduce(
+    (acc, x, i) => {
+      const sumPlusCurrentDay = acc.sumOfLastSeven + x.ZAKAZENIA_DZIENNE
+      if (i < 7) {
+        return {
+          rollingAverage: { ...acc.rollingAverage, [x.DATA_SHOW]: sumPlusCurrentDay / (i + 1) },
+          sumOfLastSeven: sumPlusCurrentDay,
+        }
+      } else {
+        const sumOfLastSeven = sumPlusCurrentDay - historicData[i - 7].ZAKAZENIA_DZIENNE
+        return {
+          rollingAverage: { ...acc.rollingAverage, [x.DATA_SHOW]: sumOfLastSeven / 7 },
+          sumOfLastSeven: sumOfLastSeven,
+        }
+      }
+    },
+    { rollingAverage: {}, sumOfLastSeven: 0 },
   )
+
+  const shownData = !shownLimit
+    ? historicData
+    : historicData.filter((day) => today.diff(toDayJs(day), 'day') <= shownLimit)
+    
   const options = {
     title: {
       text: '',
@@ -21,64 +42,66 @@ function Charts() {
     rangeSelector: {
       verticalAlign: 'top',
       x: 0,
-      y: 0
+      y: 0,
     },
-    yAxis: [{
-      title: {
-        text: 'Liczba zakażeń'
+    yAxis: [
+      {
+        title: {
+          text: 'Liczba zakażeń',
+        },
       },
-    }],
+    ],
     xAxis: {
       labels: {
-        format: '{value:%b %e}'
+        format: '{value:%b %e}',
       },
       type: 'datetime',
     },
     series: [
       {
         name: 'Liczba nowych zakażeń',
-        data: shownData.map((day) => [
-          dayjs(day.DATA_SHOW, 'DD.MM.YYYY HH:mm').valueOf(),
-          day.ZAKAZENIA_DZIENNE,
-        ]),
+        data: shownData.map((day) => [toDayJs(day).valueOf(), day.ZAKAZENIA_DZIENNE]),
       },
       {
         name: 'Średnia liczba zakażeń z 7 dni',
-        data: shownData.map((day, index) => [
-          dayjs(day.DATA_SHOW, 'DD.MM.YYYY HH:mm').valueOf(),
-          index < 7 ? 0 : (shownData.slice(index - 7, index).map(x => x.ZAKAZENIA_DZIENNE).reduce((a, b) => a + b) / 7)
-        ]),
+        data: shownData.map((day) => [toDayJs(day).valueOf(), rollingAverage[day.DATA_SHOW]]),
       },
     ],
   }
 
   return (
-    <Jumbotron className="p-0 m-2">
+    <Jumbotron className='p-0 m-2'>
       <div className='text-center'>
         <h2>Wykres nowych zakażeń</h2>
       </div>
-      <Container className="text-center p-4">
+      <Container className='text-center p-4'>
         <Row>
           <Col>
             <div className='text-center'>
               <h7>Zakres pokazywanych danych</h7>
               <div className='timeline-controls'>
-                {
-                  [
-                    // [null, 'Od zarania dziejów'],
-                    [365, 'Ostatni rok'],
-                    [31, 'Ostatni miesciąc'],
-                    [7, 'Ostatni tydzień'],
-                  ].map(([value, label]) => (
-                    <button
-                      key={value}
-                      className={shownLimit === value ? 'btn btn-primary' : 'btn btn-outline-primary'}
-                      onClick={() => {
-                        setShownLimit(value)
-                      }}
-                    >{label}</button>
-                  ))
-                }
+                <Container>
+                  <Row>
+                    {[
+                      [null, 'Wszystkie dane'],
+                      [365, 'Ostatni rok'],
+                      [93, 'Ostatni kwartał'],
+                      [31, 'Ostatni miesciąc'],
+                      [7, 'Ostatni tydzień'],
+                    ].map(([value, label]) => (
+                      <Col lg={{ span: 2 }} md={{ span: 4 }} xs={{ span: 6 }} key={value}>
+                        <button
+                          className={shownLimit === value ? 'btn btn-primary' : 'btn btn-outline-primary'}
+                          onClick={() => {
+                            setShownLimit(value)
+                          }}
+                        >
+                          {label}
+                        </button>
+                      </Col>
+                    ))}
+                  </Row>
+                </Container>
               </div>
             </div>
           </Col>
